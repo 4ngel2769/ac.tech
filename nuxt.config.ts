794ee6c92902e06b1fdce2bca2efaa5d8c3f1b1b
Sypher0 <78003275+4ngel2769@users.noreply.github.com>
2024-04-18 18:06:38 +0300

    moved stylesheet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,8 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://www.nerdfonts.com/assets/css/webfont.css' },
         // Google's material icons
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap'}
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap'},
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap'}
       ]
     }
   },
@@ -55,10 +56,10 @@ export default defineNuxtConfig({
   // linkChecker: {
   //   enabled: true
   // },
-  site: {
-    url: 'https://angelcapra.tech',
-    // name: 'Angel 🚀',
-    // description: 'Personal website; find my projects, social links, and more. 📄',
-    defaultLocale: 'en',
-  }
+  // site: {
+  //   url: 'https://angelcapra.tech',
+  //   // name: 'Angel 🚀',
+  //   // description: 'Personal website; find my projects, social links, and more. 📄',
+  //   defaultLocale: 'en',
+  // }
 })
